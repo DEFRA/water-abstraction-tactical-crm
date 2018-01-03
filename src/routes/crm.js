4ec25682875151119f36aa946c1a716e9fe35347
James Carmichael <james@Jamess-MacBook-Pro.local>
2018-01-03 16:28:53 +0000

    Starting to use REST API for more routes

--- src/routes/crm.js
+++ src/routes/crm.js
@@ -11,6 +11,8 @@ const Joi = require('joi');
 const Helpers = require('../lib/helpers');
 
 const HAPIRestAPI = require('../lib/rest-api');
+
+
 const VerificationApi = new HAPIRestAPI({
   table : 'crm.verification',
   primaryKey : 'verification_id',
@@ -31,6 +33,27 @@ const VerificationApi = new HAPIRestAPI({
   }
 });
 
+
+const DocumentHeaderApi = new HAPIRestAPI({
+  table : 'crm.document_header',
+  primaryKey : 'document_id',
+  endpoint : '/crm/' + version + '/documentHeader',
+  validation : {
+    document_id : Joi.string().guid(),
+    regime_entity_id : Joi.string().guid(),
+    system_id : Joi.string(),
+    system_internal_id : Joi.string(),
+    system_external_id : Joi.string(),
+    metadata : Joi.string(),
+    company_entity_id : Joi.string().guid(),
+    verified : Joi.number(),
+    verification_id : Joi.string().guid()
+  }
+});
+
+
+
+
 module.exports = [
   { method: 'GET', path: '/status', handler: function(request,reply){return reply('ok').code(200)}, config:{auth: false,description:'Get all entities'}},
   { method: 'GET', path: '/crm/' + version + '/entity', handler: CRM.getAllEntities, config:{description:'Get all entities'} },
@@ -48,8 +71,15 @@ module.exports = [
   {  method: 'GET', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.getEntityAssociation ,config:{description:'Get specified association'}},
   {  method: 'PUT', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.updateEntityAssociation ,config:{description:'Update specified association'}},
   {  method: 'DELETE', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.deleteEntityAssociation ,config:{description:'Delete specified association'}},
-  {  method: 'GET', path: '/crm/' + version + '/documentHeader', handler: CRM.getDocumentHeaders ,config:{description:'Get all document headers'}},
+
+
+
+  DocumentHeaderApi.getRoutes()[0],
+  // {  method: 'GET', path: '/crm/' + version + '/documentHeader', handler: CRM.getDocumentHeaders ,config:{description:'Get all document headers'}},
+
   {  method: 'POST', path: '/crm/' + version + '/documentHeader', handler: CRM.createDocumentHeader ,config:{description:'Create new document header'}},
+
+
   {  method: 'GET', path: '/crm/' + version + '/documentHeader/{document_id}/entity/{entity_id}/name', handler: CRM.getDocumentNameForUser ,config:{description:'Get custom name for document'}},
   {  method: 'POST', path: '/crm/' + version + '/documentHeader/{document_id}/entity/{entity_id}/name', handler: CRM.setDocumentNameForUser ,config:{description:'Set custom name for document'}},
   {  method: 'PATCH', path: '/crm/' + version + '/documentHeaders', handler: CRM.updateDocumentHeaders, config : {
@@ -74,7 +104,7 @@ module.exports = [
   {  method: 'PUT', path: '/crm/' + version + '/documentHeader/{system_id}/{system_internal_id}', handler: CRM.updateDocumentHeader, config:{description:'Update specified document header by external system & external system document id'} },
   {  method: 'DELETE', path: '/crm/' + version + '/documentHeader/{document_id}', handler: CRM.deleteDocumentHeader ,config:{description:'Delete specified document header by document id'}},
   {  method: 'DELETE', path: '/crm/' + version + '/documentHeader/{system_id}/{system_internal_id}', handler: CRM.deleteDocumentHeader ,config:{description:'Delete specified document header by external system & external system document id'}},
-  {  method: 'POST', path: '/crm/' + version + '/documentHeader/filter', handler: CRM.getDocumentHeaders ,config:{description:'Search for document headers by posted filter criteria'}},
+  {  method: 'POST', path: '/crm/' + version + '/documentHeader/filter', handler: CRM.getRoleDocuments ,config:{description:'Search for document headers by posted filter criteria'}},
   {  method: 'PUT', path: '/crm/' + version + '/documentHeader/{document_id}/owner', handler: CRM.setDocumentOwner ,config:{description:'Search for document headers by posted filter criteria'}},
   {  method: 'POST', path: '/crm/' + version + '/entity/{entity_id}/roles', handler: CRM.addEntityRole ,config:{description:'Add role to specified entity'}},
   {  method: 'GET', path: '/crm/' + version + '/entity/{entity_id}/roles', handler: CRM.getEntityRoles ,config:{description:'Get roles for specified entity'}},
