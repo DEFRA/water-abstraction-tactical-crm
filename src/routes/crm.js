5c45d60f3473ed076f7cc730361139174b29e83a
James Carmichael <james@siteclick.co.uk>
2018-01-15 13:23:11 +0000

    Replace bulk patch documentheaders with REST API version

--- src/routes/crm.js
+++ src/routes/crm.js
@@ -44,40 +44,15 @@ module.exports = [
   {  method: 'DELETE', path: '/crm/' + version + '/entity/{entity_id}/colleagues/{role_id}', handler: CRM.deleteColleague ,config:{description:'Remove specified colleague of entity'}},
   {  method: 'POST', path: '/crm/' + version + '/entity/{entity_id}/colleagues', handler: CRM.createColleague ,config:{description:'Create new colleague of entity'}},
 
-  // {  method: 'GET', path: '/crm/' + version + '/entityAssociation', handler: CRM.getEntityAssociations ,config:{description:'Get associations for specified entity'}},
-  // {  method: 'POST', path: '/crm/' + version + '/entityAssociation', handler: CRM.createEntityAssociation ,config:{description:'Add association for specified entity'}},
-  // {  method: 'GET', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.getEntityAssociation ,config:{description:'Get specified association'}},
-  // {  method: 'PUT', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.updateEntityAssociation ,config:{description:'Update specified association'}},
-  // {  method: 'DELETE', path: '/crm/' + version + '/entityAssociation/{entity_association_id}', handler: CRM.deleteEntityAssociation ,config:{description:'Delete specified association'}},
-
   // DocumentHeaderApi.getRoutes()[0],
-
   // {  method: 'POST', path: '/crm/' + version + '/documentHeader', handler: CRM.createDocumentHeader ,config:{description:'Create new document header'}},
 
   {  method: 'GET', path: '/crm/' + version + '/documentHeader/{document_id}/entity/{entity_id}/name', handler: CRM.getDocumentNameForUser ,config:{description:'Get custom name for document'}},
   {  method: 'POST', path: '/crm/' + version + '/documentHeader/{document_id}/entity/{entity_id}/name', handler: CRM.setDocumentNameForUser ,config:{description:'Set custom name for document'}},
-  {  method: 'PATCH', path: '/crm/' + version + '/documentHeaders', handler: CRM.updateDocumentHeaders, config : {
-    description: 'Bulk update a set of document headers based on supplied query conditions',
-    validate: {
-      payload : {
-        query : {
-          verification_id : Joi.string().guid(),
-          document_id : Joi.array()
-        },
-        set : {
-          verification_id : Joi.string().guid(),
-          verified : Joi.number(),
-          company_entity_id : Joi.string().guid()
-        }
-      }
-    }
-  }},
 
 
   // Document header
   ...DocumentHeaderApi.getRoutes(),
-
-
 
 
   // {  method: 'GET', path: '/crm/' + version + '/documentHeader/{document_id}', handler: CRM.getDocumentHeader ,config:{description:'Get specified document header by document id'}},
