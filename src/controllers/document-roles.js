2ec665508c0a9ca042dbf2d9ce4e4d6f3a6dfb50
Dave Cozens <davecozens.defra@gmail.com>
2018-02-13 13:38:50 +0000

    role_document_access view exposed at document_role_access endpoint to enable finding primary user for a document

---
+++ src/controllers/document-roles.js
@@ -0,0 +1,20 @@
+const HAPIRestAPI = require('hapi-pg-rest-api');
+const Joi = require('joi');
+
+module.exports = (config = {}) => {
+  const {pool, version} = config;
+  return new HAPIRestAPI({
+    table : 'crm.role_document_access',
+    primaryKey : 'entity_role_id',
+    endpoint : '/crm/' + version + '/document_role_access',
+    connection : pool,
+    validation : {
+      entity_role_id : Joi.string().guid(),
+      role : Joi.string(),
+      system_external_id : Joi.string(),
+      filter:Joi.object(),
+      sort:Joi.object(),
+      pagination:Joi.object(),
+    }
+  });
+}
