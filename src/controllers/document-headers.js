e1be11b095addd74716cc63d293131e8f41468a9
James Carmichael <james@siteclick.co.uk>
2018-04-11 16:57:40 +0100

    Improve readability

--- src/controllers/document-headers.js
+++ src/controllers/document-headers.js
@@ -1,5 +1,6 @@
 const HAPIRestAPI = require('hapi-pg-rest-api');
 const Joi = require('joi');
+const { pool } = require('../lib/connectors/db');
 
 /**
  * Function to map a data row from the roles table into a mongo-sql
@@ -36,69 +37,104 @@ const getSearchFilter = (string) => {
    ];
 }
 
+/**
+ * Gets a list of roles from the DB for supplied email address
+ * @param {String} email
+ * @return {Array} entity_role records
+ */
+async function getRolesForEmail(email) {
+  const query = `SELECT r.* FROM crm.entity e
+            JOIN crm.entity_roles r ON e.entity_id=r.entity_id
+            WHERE LOWER(e.entity_nm)=LOWER($1) AND e.entity_type='individual' `;
+  const { rows, error } = await pool.query(query, [email]);
+  if(error) {
+    throw error;
+  }
+  return rows;
+}
+
+/**
+ * Get roles for entity ID
+ * @params {String} entityId - individual
+ * @return {Array} entity_role records
+ */
+ async function getRolesForIndividual(entityId) {
+   const query = `SELECT * FROM crm.entity_roles WHERE entity_id=$1`;
+   let { rows, error } = await pool.query(query, [entityId]);
+   if(error) {
+     throw error;
+   }
+   return rows;
+ }
+
+
+
+/**
+ * Get email filter from email string
+ * @param {String} email
+ * @return {Promise} resolves with mongo-sql query fragment
+ */
+const getEmailFilter = async (email) => {
+  const emailRoles = await getRolesForEmail(email);
+  if(emailRoles.length === 0) {
+    return {$or : {company_entity_id : 'no-entity-found-with-email'}};
+  }
+  return { $or : emailRoles.map(mapRole) };
+}
+
+/**
+ * Get entity filter from individual entity ID GUID
+ * @param {String} entityId - GUID
+ * @return {Promise} resolves with mongo-sql query fragment
+ */
+const getEntityFilter = async (entityId) => {
+  const entityRoles = await getRolesForIndividual(entityId);
+  if(entityRoles.length === 0) {
+    return {$or : {company_entity_id : 'no-entity-found-with-id'}};
+  }
+  return { $or : entityRoles.map(mapRole) };
+}
+
+
+async function preQuery (result, hapiRequest) {
+
+ const { string, email, entity_id : entityId, ...filter } = result.filter;
+ const { document_expires, ...sort } = result.sort;
+
+ // Search by string - can be licence number/name
+ if(string) {
+   filter.$or = getSearchFilter(string);
+ };
+
+ // Search by entity ID / entity email address (can be combined)
+ if(email || entityId) {
+   filter.$and = [];
+ }
+
+ if(email) {
+   filter.$and.push(await getEmailFilter(email));
+ }
+
+ if(entityId) {
+   filter.$and.push(await getEntityFilter(entityId));
+ }
+
+ // Rewrite sort
+ if(document_expires) {
+   sort["metadata->>Expires"] = document_expires;
+ }
+
+ result.filter = filter;
+ result.sort = sort;
+
+ console.log(JSON.stringify(result, null, 2));
+
+ return result;
+}
+
 
 module.exports = (config = {}) => {
   const {pool, version} = config;
-
-  /**
-   * Gets a list of roles from the DB for supplied email address
-   * @param {String} email
-   * @return {Array} entity_role records
-   */
-  async function getRolesForEmail(email) {
-    const query = `SELECT r.* FROM crm.entity e
-              JOIN crm.entity_roles r ON e.entity_id=r.entity_id
-              WHERE LOWER(e.entity_nm)=LOWER($1) AND e.entity_type='individual' `;
-    const { rows, error } = await pool.query(query, [email]);
-    if(error) {
-      throw error;
-    }
-    return rows;
-  }
-
-  /**
-   * Get roles for entity ID
-   * @params {String} entityId - individual
-   * @return {Array} entity_role records
-   */
-   async function getRolesForIndividual(entityId) {
-     const query = `SELECT * FROM crm.entity_roles WHERE entity_id=$1`;
-     let { rows, error } = await pool.query(query, [entityId]);
-     if(error) {
-       throw error;
-     }
-     return rows;
-   }
-
-
-
-  /**
-   * Get email filter from email string
-   * @param {String} email
-   * @return {Promise} resolves with mongo-sql query fragment
-   */
-  const getEmailFilter = async (email) => {
-    const emailRoles = await getRolesForEmail(email);
-    if(emailRoles.length === 0) {
-      return {$or : {company_entity_id : 'no-entity-found-with-email'}};
-    }
-    return { $or : emailRoles.map(mapRole) };
-  }
-
-  /**
-   * Get entity filter from individual entity ID GUID
-   * @param {String} entityId - GUID
-   * @return {Promise} resolves with mongo-sql query fragment
-   */
-  const getEntityFilter = async (entityId) => {
-    const entityRoles = await getRolesForIndividual(entityId);
-    if(entityRoles.length === 0) {
-      return {$or : {company_entity_id : 'no-entity-found-with-id'}};
-    }
-    return { $or : entityRoles.map(mapRole) };
-  }
-
-
 
   return new HAPIRestAPI({
     table : 'crm.document_header',
@@ -111,41 +147,7 @@ module.exports = (config = {}) => {
       set : ['system_external_id', 'metadata']
     },
 
-    preQuery : async (result, hapiRequest) => {
-
-      const { string, email, entity_id : entityId, ...filter } = result.filter;
-      const { document_expires, ...sort } = result.sort;
-
-      // Search by string - can be licence number/name
-      if(string) {
-        filter.$or = getSearchFilter(string);
-      };
-
-      // Search by entity ID / entity email address (can be combined)
-      if(email || entityId) {
-        filter.$and = [];
-      }
-
-      if(email) {
-        filter.$and.push(await getEmailFilter(email));
-      }
-
-      if(entityId) {
-        filter.$and.push(await getEntityFilter(entityId));
-      }
-
-      // Rewrite sort
-      if(document_expires) {
-        sort["metadata->>Expires"] = document_expires;
-      }
-
-      result.filter = filter;
-      result.sort = sort;
-
-      console.log(JSON.stringify(result, null, 2));
-
-      return result;
-    },
+    preQuery,
 
     validation : {
       document_id : Joi.string().guid(),
