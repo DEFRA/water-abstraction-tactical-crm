3d0f150765fdc7f834903ed0d559e50123f7c5d0
Mark Swaffer <mdswaffer@gmail.com>
2019-04-11 16:54:08 +0100

    Feat: Allow expired documents to be found (#213)
    WATER-1976
    
    Updates the document header pre filter to allow expired licences to be
    returned if the query sets `includeExpired` to true.

--- test/controllers/document-headers.js
+++ test/controllers/document-headers.js
@@ -2,12 +2,24 @@
  * Tests that users can view documents depending on their roles within a company
  */
 'use strict';
-const Lab = require('lab');
-const lab = exports.lab = Lab.script();
-const Code = require('code');
+const {
+  before,
+  after,
+  experiment,
+  test
+} = exports.lab = require('lab').script();
+const { expect } = require('code');
 const server = require('../../index');
 
-const { createDocumentHeader, createEntity, deleteEntity, deleteDocumentHeader, createEntityRole, deleteEntityRole } = require('../helpers');
+const {
+  createDocumentHeader,
+  createEntity,
+  deleteEntity,
+  deleteDocumentHeader,
+  createEntityRole,
+  deleteEntityRole
+} = require('../helpers');
+
 const { getSearchFilter } = require('../../src/controllers/document-headers');
 
 let unclaimedDocumentId;
@@ -16,6 +28,7 @@ let regimeEntityId;
 let companyEntityId;
 let individualEntityId;
 let entityRoleId;
+let expiredDocument;
 
 const createDocumentRequest = (filter = {}) => {
   const filterStr = JSON.stringify(filter);
@@ -31,8 +44,8 @@ const createDocumentRequest = (filter = {}) => {
   return server.inject(request);
 };
 
-lab.experiment('Test document filter', () => {
-  lab.before(async() => {
+experiment('Test document filter', () => {
+  before(async() => {
       // Create entities
     {
       const { entity_id: entityId } = await createEntity('regime');
@@ -54,51 +67,52 @@ lab.experiment('Test document filter', () => {
       const { document_id: documentId } = await createDocumentHeader(regimeEntityId, companyEntityId);
       claimedDocumentId = documentId;
     }
+    expiredDocument = await createDocumentHeader(regimeEntityId, companyEntityId, false);
     const entityRole = await createEntityRole(regimeEntityId, companyEntityId, individualEntityId);
     entityRoleId = entityRole.entity_role_id;
   });
 
-  lab.test('The document filter should find document by ID', async () => {
+  test('The document filter should find document by ID', async () => {
     const filter = {
       document_id: unclaimedDocumentId
     };
     const res = await createDocumentRequest(filter);
-    Code.expect(res.statusCode).to.equal(200);
+    expect(res.statusCode).to.equal(200);
 
     // Check payload
     const payload = JSON.parse(res.payload);
-    Code.expect(payload.data[0].document_id).to.equal(unclaimedDocumentId);
+    expect(payload.data[0].document_id).to.equal(unclaimedDocumentId);
   });
 
-  lab.test('The document filter should not find document when entity has no role on company', async () => {
+  test('The document filter should not find document when entity has no role on company', async () => {
     const filter = {
       document_id: unclaimedDocumentId,
       entity_id: individualEntityId
     };
 
     const res = await createDocumentRequest(filter);
-    Code.expect(res.statusCode).to.equal(200);
+    expect(res.statusCode).to.equal(200);
 
     // Check payload
     const payload = JSON.parse(res.payload);
-    Code.expect(payload.data.length).to.equal(0);
+    expect(payload.data.length).to.equal(0);
   });
 
-  lab.test('The document filter should find document when entity has any role on company', async () => {
+  test('The document filter should find document when entity has any role on company', async () => {
     const filter = {
       document_id: claimedDocumentId,
       entity_id: individualEntityId
     };
 
     const res = await createDocumentRequest(filter);
-    Code.expect(res.statusCode).to.equal(200);
+    expect(res.statusCode).to.equal(200);
 
     // Check payload
     const payload = JSON.parse(res.payload);
-    Code.expect(payload.data[0].document_id).to.equal(claimedDocumentId);
+    expect(payload.data[0].document_id).to.equal(claimedDocumentId);
   });
 
-  lab.test('The document filter should find document when entity has specific role on company', async () => {
+  test('The document filter should find document when entity has specific role on company', async () => {
     const filter = {
       document_id: claimedDocumentId,
       entity_id: individualEntityId,
@@ -106,14 +120,14 @@ lab.experiment('Test document filter', () => {
     };
 
     const res = await createDocumentRequest(filter);
-    Code.expect(res.statusCode).to.equal(200);
+    expect(res.statusCode).to.equal(200);
 
     // Check payload
     const payload = JSON.parse(res.payload);
-    Code.expect(payload.data[0].document_id).to.equal(claimedDocumentId);
+    expect(payload.data[0].document_id).to.equal(claimedDocumentId);
   });
 
-  lab.test('The document filter should not find document when entity does not have specific role on company', async () => {
+  test('The document filter should not find document when entity does not have specific role on company', async () => {
     const filter = {
       document_id: claimedDocumentId,
       entity_id: individualEntityId,
@@ -121,14 +135,26 @@ lab.experiment('Test document filter', () => {
     };
 
     const res = await createDocumentRequest(filter);
-    Code.expect(res.statusCode).to.equal(200);
+    expect(res.statusCode).to.equal(200);
 
     // Check payload
     const payload = JSON.parse(res.payload);
-    Code.expect(payload.data.length).to.equal(0);
+    expect(payload.data.length).to.equal(0);
   });
 
-  lab.after(async() => {
+  test('returns an expired document if requested', async () => {
+    const filter = {
+      string: expiredDocument.system_external_id,
+      includeExpired: true
+    };
+
+    const res = await createDocumentRequest(filter);
+    const payload = JSON.parse(res.payload);
+    expect(payload.data.length).to.equal(1);
+    expect(payload.data[0].document_id).to.equal(expiredDocument.document_id);
+  });
+
+  after(async() => {
     // Delete all temporary entities
     const entityIds = [regimeEntityId, individualEntityId, companyEntityId];
     await Promise.all(entityIds, (entityId) => {
@@ -144,10 +170,10 @@ lab.experiment('Test document filter', () => {
   });
 });
 
-lab.experiment('getSearchFilter', () => {
-  lab.test('It should format a filter object to search by licence number, document name, or licence holder', async() => {
+experiment('getSearchFilter', () => {
+  test('It should format a filter object to search by licence number, document name, or licence holder', async() => {
     const result = getSearchFilter('Test');
-    Code.expect(result).to.equal([
+    expect(result).to.equal([
       {
         system_external_id: {
           $ilike: `%Test%`
