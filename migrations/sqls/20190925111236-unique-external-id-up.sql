c088ae6beab649b0cc72028d372515275e042e50
James Carmichael <james@siteclick.co.uk>
2019-09-26 13:23:18 +0100

    Migration tweaks

---
+++ migrations/sqls/20190925111236-unique-external-id-up.sql
@@ -0,0 +1,46 @@
+/* Replace with your SQL commands */
+ALTER TABLE "crm_v2"."companies"
+  ADD CONSTRAINT unique_external_id UNIQUE (external_id);
+
+ALTER TABLE "crm_v2"."companies"
+  ALTER COLUMN type DROP NOT NULL;
+
+ALTER TABLE "crm_v2"."contacts"
+  ADD CONSTRAINT unique_contact_external_id UNIQUE (external_id);
+
+ALTER TABLE "crm_v2"."contacts"
+  ADD COLUMN initials VARCHAR;
+
+INSERT INTO "crm_v2"."roles"
+  (name, date_created, date_updated)
+  VALUES ('billing', NOW(), NOW())
+
+ALTER TABLE "crm_v2"."company_contacts"
+  DROP CONSTRAINT company_role_contact;
+
+ALTER TABLE "crm_v2"."company_contacts"
+  ADD CONSTRAINT company_role_contact UNIQUE (company_id, contact_id, role_id, start_date);
+
+ALTER TABLE "crm_v2"."invoice_accounts"
+  ALTER COLUMN start_date DROP NOT NULL;
+
+ALTER TABLE "crm_v2"."invoice_accounts"
+  ADD CONSTRAINT invoice_account_company_ias UNIQUE (company_id, ias_account_number);
+
+ALTER TABLE "crm_v2"."addresses"
+  ADD CONSTRAINT unique_address_external_id UNIQUE (external_id)
+
+ALTER TABLE "crm_v2"."addresses"
+  ALTER COLUMN county DROP NOT NULL;
+
+ALTER TABLE "crm_v2"."addresses"
+  ALTER COLUMN country DROP NOT NULL;
+
+ALTER TABLE "crm_v2"."addresses"
+  ALTER COLUMN country TYPE varchar;
+
+ALTER TABLE "crm_v2"."addresses"
+  ALTER COLUMN postcode DROP NOT NULL;
+
+ALTER TABLE "crm_v2"."addresses"
+  ALTER COLUMN town DROP NOT NULL;
