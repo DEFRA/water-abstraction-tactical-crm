dc7b1417c3360d74457a822f067cb70660697dc2
James Carmichael <james@siteclick.co.uk>
2019-10-10 14:25:47 +0100

    Add tests for repository classes

---
+++ test/v2/connectors/repository/DocumentsRepository.js
@@ -0,0 +1,51 @@
+const { experiment, test, beforeEach, afterEach } = exports.lab = require('@hapi/lab').script();
+const { expect } = require('@hapi/code');
+const sandbox = require('sinon').createSandbox();
+const { pool } = require('../../../../src/lib/connectors/db');
+
+const queries = require('../../../../src/v2/connectors/repository/queries/documents');
+const DocumentsRepository = require('../../../../src/v2/connectors/repository/DocumentsRepository');
+
+experiment('DocumentsRepository', () => {
+  let repo;
+  const response = {
+    rows: [{ foo: 'bar' }]
+  };
+
+  beforeEach(async () => {
+    sandbox.stub(pool, 'query').resolves(response);
+    repo = new DocumentsRepository();
+  });
+
+  afterEach(async () => {
+    sandbox.restore();
+  });
+
+  experiment('findOneById', async () => {
+    test('calls pool.query with correct query and params', async () => {
+      await repo.findOneById('document_1');
+      const [query, params] = pool.query.lastCall.args;
+      expect(query).to.equal(queries.findOneById);
+      expect(params).to.equal(['document_1']);
+    });
+
+    test('resolves with single row data', async () => {
+      const result = await repo.findOneById('document_1');
+      expect(result).to.equal(response.rows[0]);
+    });
+  });
+
+  experiment('findByDocumentRef', async () => {
+    test('calls pool.query with correct query and params', async () => {
+      await repo.findByDocumentRef('water', 'abstraction_licence', 'doc_1');
+      const [query, params] = pool.query.lastCall.args;
+      expect(query).to.equal(queries.findByDocumentRef);
+      expect(params).to.equal(['water', 'abstraction_licence', 'doc_1']);
+    });
+
+    test('resolves with row data', async () => {
+      const result = await repo.findByDocumentRef('document_1');
+      expect(result).to.equal(response.rows);
+    });
+  });
+});
