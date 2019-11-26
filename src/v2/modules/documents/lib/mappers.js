86d12c1010690d6961b4682aa09831433997a49a
James Carmichael <james@siteclick.co.uk>
2019-11-26 13:16:51 +0000

    Fixes an issue where billing roles were not retrieved for documents

--- src/v2/modules/documents/lib/mappers.js
+++ src/v2/modules/documents/lib/mappers.js
@@ -10,9 +10,26 @@ const mapEntity = (row, fields) => {
     : camelCaseKeys(pick(row, fields));
 };
 
+/**
+ * Maps a row retrieved from the DB to a company
+ * For billing role, this is the company linked to the invoice account
+ * @param {Object} row - the DB row
+ * @return {Object} company
+ */
+const mapCompany = row => {
+  if (row.role_name === 'billing') {
+    return {
+      companyId: row.invoice_company_id,
+      name: row.invoice_company_name,
+      companyNumber: row.invoice_company_number
+    };
+  }
+  return mapEntity(row, ['company_id', 'name', 'company_number']);
+};
+
 const mapDocumentRole = row => ({
   ...mapEntity(row, ['document_role_id', 'role_id', 'role_name', 'start_date', 'end_date']),
-  company: mapEntity(row, ['company_id', 'name', 'company_number']),
+  company: mapCompany(row),
   contact: mapEntity(row, ['contact_id', 'salutation', 'first_name', 'last_name', 'middle_names']),
   address: mapEntity(row, ['address_id', 'address_1', 'address_2', 'address_3', 'address_4', 'town', 'county', 'postcode', 'country']),
   invoiceAccount: mapEntity(row, ['invoice_account_id', 'invoice_account_number'])
