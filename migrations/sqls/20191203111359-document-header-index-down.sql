a484f828d2900ba274f5d2cfa77a75a859905b1f
Mark Swaffer <mdswaffer@gmail.com>
2019-12-03 12:49:28 +0000

    Fix: Improves the query time for select company queries
    Adds indexes to the table that are queried when the select company page
    is presented to an external user.

---
+++ migrations/sqls/20191203111359-document-header-index-down.sql
@@ -0,0 +1,7 @@
+drop index crm.idx_document_header_company_id;
+
+drop index crm.entity_roles_idx_entity_id;
+drop index crm.entity_roles_idx_company_id;
+
+drop index crm.verification_idx_entity_id;
+drop index crm.verification_documents_idx_document_id;
