4787bcddd5df7444084ac6a976002ec61884a888
Mark Swaffer <mdswaffer@gmail.com>
2019-12-11 10:22:25 +0000

    Feat: Get contacts from the v2 crm (#405)
    WATER-2445
    
    Adds endpoints to get a single contact from the API or alternatively get
    many contacts by passing a comma separated list of ids to the API.

--- src/v2/connectors/repository/index.js
+++ src/v2/connectors/repository/index.js
@@ -1,5 +1,7 @@
 const DocumentsRepository = require('./DocumentsRepository');
 const DocumentRolesRepository = require('./DocumentRolesRepository');
+const ContactsRepository = require('./ContactsRepository');
 
+exports.contacts = new ContactsRepository();
 exports.documents = new DocumentsRepository();
 exports.documentRoles = new DocumentRolesRepository();
