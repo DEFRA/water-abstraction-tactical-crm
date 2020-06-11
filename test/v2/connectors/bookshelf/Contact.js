d5f488381e5374b05962ece90ae8fb4c9024245f
James Carmichael <james@siteclick.co.uk>
2020-06-11 18:09:49 +0100

    Additional tests

--- test/v2/connectors/bookshelf/Contact.js
+++ test/v2/connectors/bookshelf/Contact.js
@@ -5,28 +5,28 @@ const {
 } = exports.lab = require('@hapi/lab').script();
 const { expect } = require('@hapi/code');
 
-const Address = require('../../../../src/v2/connectors/bookshelf/Address');
+const Address = require('../../../../src/v2/connectors/bookshelf/Contact');
 
-experiment('v2/connectors/bookshelf/Address', () => {
+experiment('v2/connectors/bookshelf/Contact', () => {
   let instance;
 
   beforeEach(async () => {
     instance = Address.forge();
   });
 
-  test('uses the address table', async () => {
-    expect(instance.tableName).to.equal('addresses');
+  test('uses the contacts table', async () => {
+    expect(instance.tableName).to.equal('contacts');
   });
 
   test('uses the correct ID attribute', async () => {
-    expect(instance.idAttribute).to.equal('address_id');
+    expect(instance.idAttribute).to.equal('contact_id');
   });
 
   test('uses the correct timestamp fields', async () => {
     expect(instance.hasTimestamps).to.equal(['date_created', 'date_updated']);
   });
 
-  test('has a companyAddresses relationship', async () => {
-    expect(instance.companyAddresses).to.be.a.function();
+  test('has a companyContacts relationship', async () => {
+    expect(instance.companyContacts).to.be.a.function();
   });
 });
