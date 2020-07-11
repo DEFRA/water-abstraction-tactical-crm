2012ad6347e640cf18f30cf6955ce476e1a57314
Mark Swaffer <mdswaffer@gmail.com>
2020-07-11 09:55:09 +0100

    Feat: Adds resource to get role by name
    WATER-2744
    
    Exposes a new resource to get a role entity by role name.

---
+++ src/v2/modules/roles/routes.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+
+const controller = require('./controller');
+
+exports.getRoleByName = {
+  method: 'GET',
+  path: '/crm/2.0/roles/{roleName}',
+  handler: controller.getRoleByName,
+  options: {
+    description: 'Get a role by name',
+    validate: {
+      params: {
+        roleName: Joi.string().required()
+      }
+    }
+  }
+};
