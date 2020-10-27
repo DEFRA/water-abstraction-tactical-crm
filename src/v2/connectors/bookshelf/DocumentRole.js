987d6dc6db3b8197a8a38852410f64449e5e9dca
James Carmichael <james@siteclick.co.uk>
2020-10-27 12:31:23 +0000

    Includes full company info in document roles. Removes legacy DocumentRolesRepository and uses Bookshelf.

--- src/v2/connectors/bookshelf/DocumentRole.js
+++ src/v2/connectors/bookshelf/DocumentRole.js
@@ -3,6 +3,10 @@
 const { bookshelf } = require('./bookshelf.js');
 const Role = require('./Role');
 const Document = require('./Document');
+const Company = require('./Company');
+const Address = require('./Address');
+const Contact = require('./Contact');
+const InvoiceAccount = require('./InvoiceAccount');
 
 module.exports = bookshelf.model('DocumentRole', {
   tableName: 'document_roles',
@@ -15,5 +19,21 @@ module.exports = bookshelf.model('DocumentRole', {
 
   document () {
     return this.hasOne(Document, 'document_id', 'document_id');
+  },
+
+  company () {
+    return this.hasOne(Company, 'company_id', 'company_id');
+  },
+
+  address () {
+    return this.hasOne(Address, 'address_id', 'address_id');
+  },
+
+  contact () {
+    return this.hasOne(Contact, 'contact_id', 'contact_id');
+  },
+
+  invoiceAccount () {
+    return this.hasOne(InvoiceAccount, 'invoice_account_id', 'invoice_account_id');
   }
 });
--- src/v2/connectors/repository/index.js
+++ src/v2/connectors/repository/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
-const DocumentRolesRepository = require('./DocumentRolesRepository');
-exports.documentRoles = new DocumentRolesRepository();
-
 exports.companies = require('./companies');
 exports.companyAddresses = require('./company-addresses');
 exports.companyContacts = require('./company-contacts');
+exports.documentRoles = require('./document-roles');
 exports.invoiceAccounts = require('./invoice-accounts');
 exports.invoiceAccountAddresses = require('./invoice-account-addresses');
 exports.documents = require('./documents');
