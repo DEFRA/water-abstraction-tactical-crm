b15fd7e3f2ff5002e9215cfbfd90f70aa236e190
Tim <tim@tamimi.dev>
2021-04-29 12:30:24 +0100

    Make the queries slightly lighter by copying the generated hash

--- migrations/sqls/20210429104232-entity-hash-columns-up.sql
+++ migrations/sqls/20210429104232-entity-hash-columns-up.sql
@@ -4,7 +4,7 @@ alter table crm_v2.addresses add column current_hash VARCHAR default null;
 
 /* Populate initial addresses hash values */
 update crm_v2.addresses addresses set last_hash = md5(CAST((addresses.address_1, addresses.address_2, addresses.address_3, addresses.address_4, addresses.town, addresses.county, addresses.postcode) AS text));
-update crm_v2.addresses addresses set current_hash = md5(CAST((addresses.address_1, addresses.address_2, addresses.address_3, addresses.address_4, addresses.town, addresses.county, addresses.postcode) AS text));
+update crm_v2.addresses addresses set current_hash = last_hash;
 
 
 /* Create hash columns for the crm_v2.companies entity */
@@ -13,7 +13,7 @@ alter table crm_v2.companies add column current_hash VARCHAR default null;
 
 /* Populate initial companies hash values */
 update crm_v2.companies companies set last_hash = md5(CAST((companies.name, companies.type) AS text));
-update crm_v2.companies companies set current_hash = md5(CAST((companies.name, companies.type) AS text));
+update crm_v2.companies companies set current_hash = last_hash;
 
 
 /* Create hash columns for the crm_v2.contacts entity */
@@ -22,4 +22,4 @@ alter table crm_v2.contacts add column current_hash VARCHAR default null;
 
 /* Populate initial contacts hash values */
 update crm_v2.contacts contacts set last_hash = md5(CAST((contacts.salutation, contacts.first_name, contacts.last_name) AS text));
-update crm_v2.contacts contacts set current_hash = md5(CAST((contacts.salutation, contacts.first_name, contacts.last_name) AS text));
+update crm_v2.contacts contacts set current_hash = last_hash;
