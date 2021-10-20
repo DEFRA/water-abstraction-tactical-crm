2a60c1062ba6f6c5cbd9a76fcfa63a8a34c4894e
Tim <tim@tamimi.dev>
2021-10-20 09:51:35 +0100

    Update companies.js

--- src/v2/connectors/repository/queries/companies.js
+++ src/v2/connectors/repository/queries/companies.js
@@ -8,6 +8,6 @@ order by name ASC
 exports.findLicencesByCompanyId = `
   select documents.* from crm_v2.documents 
   join crm_v2.document_roles document_roles on document_roles.document_id = documents.document_id
-  join crm_v2.roles r on r.role_id = dr.role_id
+  join crm_v2.roles r on r.role_id = document_roles.role_id
   where document_roles.company_id = :companyId 
 `;
