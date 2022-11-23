436998c95b0c10189a2ca210d865bb78d996b0c3
Rebecca Ransome <beckyrose200@aol.com>
2022-11-23 16:14:40 +0000

    Removing lodash sortBy method

--- src/v2/mappers/invoice-account.js
+++ src/v2/mappers/invoice-account.js
@@ -1,10 +1,15 @@
 const moment = require('moment')
-const { sortBy } = require('lodash')
 
 const getStartDateTimestamp = invoiceAccountAddress => moment(invoiceAccountAddress.startDate).unix()
 
 const mostRecentAddressOnly = invoiceAccount => {
-  const sortedAddresses = sortBy(invoiceAccount.invoiceAccountAddresses, getStartDateTimestamp)
+  const sortedAddresses = invoiceAccount.invoiceAccountAddresses.sort(function (startDate1, startDate2) {
+    if ((startDate1, getStartDateTimestamp) > (startDate2, getStartDateTimestamp)) {
+      return -1
+    } else {
+      return 1
+    }
+  })
 
   return {
     ...invoiceAccount,
