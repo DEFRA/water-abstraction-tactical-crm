ee7f105e761c57798990a40569e42715cf812d5c
Rebecca Ransome <beckyrose200@aol.com>
2022-11-28 09:47:10 +0000

    Adding regex comment

--- src/lib/camel-case-keys.js
+++ src/lib/camel-case-keys.js
@@ -8,6 +8,12 @@ const deepMapKeys = require('deep-map-keys')
 const camelCaseKeys = data => {
   return deepMapKeys(data, toCamelCase)
 }
+/* This regex is converting any string into Camel Case
+ * [^a-zA-Z0-9] Is the first part and it is matching any character except those inside
+ * the square brackets. This could be a dash (-), space ( ), or underscore (_)
+ * The next part of the regex +(.) is matching the first character after the dash, space or underscore.
+ * The last part is the /g. This replaces all the global matches and not just the first match, so the whole string can be converted
+*/
 
 function toCamelCase (key) {
   key.toLowerCase()
