ca5457203ed9c66e0c44b3a8bbfb5c2160505650
Jason Claxton <jason.claxton@defra.gov.uk>
2023-06-07 14:14:21 +0100

    Fix linting issue

--- src/v2/connectors/repository/queries/companies.js
+++ src/v2/connectors/repository/queries/companies.js
@@ -1,4 +1,3 @@
-
 exports.findByCompanyNameWithSoftSearch = `
 select companies.company_id, companies.name, companies.type from crm_v2.companies companies
 where (:soft IS FALSE AND UPPER(name)=UPPER(:name)) OR (:soft IS TRUE AND UPPER(name) LIKE '%' || UPPER(:name) || '%')
