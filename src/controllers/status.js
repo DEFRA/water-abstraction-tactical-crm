ae06e04d6fbea20da69849bfb4d20c2ed45807ce
Jason Claxton <jason.claxton@defra.gov.uk>
2023-08-22 16:15:22 +0100

    Update /status endpoint output

--- src/controllers/status.js
+++ src/controllers/status.js
@@ -1,7 +1,9 @@
-const pkg = require('../../package.json')
+'use strict'
 
-const statusResponse = { version: pkg.version }
+async function getStatus () {
+  return { status: 'alive' }
+}
 
-const getStatus = () => statusResponse
-
-exports.getStatus = getStatus
+module.exports = {
+  getStatus
+}
