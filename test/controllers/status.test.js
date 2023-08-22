5005ee1ef5e363465bf49adcb6f8663d8e6c7c81
Jason Claxton <jason.claxton@defra.gov.uk>
2023-08-22 16:27:34 +0100

    Fix test

--- test/controllers/status.test.js
+++ test/controllers/status.test.js
@@ -4,9 +4,9 @@ const controller = require('../../src/controllers/status')
 
 experiment('controllers/status', () => {
   experiment('getStatus', () => {
-    test('returns an object with the application version', () => {
+    test('returns an object with the application status', () => {
       const response = controller.getStatus()
-      expect(response.version).to.match(/^\d*\.\d*\.\d*$/)
+      expect(response.status).to.equal('alive')
     })
   })
 })
