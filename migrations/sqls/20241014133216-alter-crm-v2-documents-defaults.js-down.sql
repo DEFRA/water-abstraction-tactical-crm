24ebcbf7e4590632bc1e654c7286646972114022
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-10-15 10:09:00 +0100

    Update migrations/sqls/20241014133216-alter-crm-v2-documents-defaults.js-down.sql
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- migrations/sqls/20241014133216-alter-crm-v2-documents-defaults.js-down.sql
+++ migrations/sqls/20241014133216-alter-crm-v2-documents-defaults.js-down.sql
@@ -1,5 +1,9 @@
+BEGIN;
+
 -- Drop unique index on the licence number
-drop index crm_v2.documents_only_one_document_ref;
+DROP INDEX crm_v2.documents_only_one_document_ref;
 
 -- Create unique index on licence number
-create unique index documents_document_ref on crm_v2.documents(regime, document_type, document_ref);
+CREATE UNIQUE INDEX documents_document_ref ON crm_v2.documents(regime, document_type, document_ref);
+
+COMMIT;
